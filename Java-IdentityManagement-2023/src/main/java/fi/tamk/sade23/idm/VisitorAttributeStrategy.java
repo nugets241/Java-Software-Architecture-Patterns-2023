cf3f4bb5bc30de59418642baf534e6f9cfa66702
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-03-29 22:43:29 +0200

    Add VisitorAttributeStrategy class

---
+++ Java-IdentityManagement-2023/src/main/java/fi/tamk/sade23/idm/VisitorAttributeStrategy.java
@@ -0,0 +1,29 @@
+package fi.tamk.sade23.idm;
+
+public class VisitorAttributeStrategy implements IdentityAttributeStrategy {
+    private String userType;
+    private String firstName;
+    private String lastName;
+    private String institution;
+    private String contractEndDate;
+    private String personalEmail;
+
+    public VisitorAttributeStrategy(String userType, String firstName, String lastName, String institution, String contractEndDate, String personalEmail) {
+        this.userType = userType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.institution = institution;
+        this.contractEndDate = contractEndDate;
+        this.personalEmail = personalEmail;
+    }
+
+    @Override
+    public void setAttributes(Identity identity, String password) {
+        identity.setUserType(userType);
+        identity.setFirstName(firstName);
+        identity.setLastName(lastName);
+        identity.setInstitution(institution);
+        identity.setContractEndDate(contractEndDate);
+        identity.setPersonalEmail(personalEmail);
+    }
+}
