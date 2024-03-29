0e6ab493e24d5faea4d9a30d691221033d0f25ac
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-03-29 22:42:15 +0200

    Add Contact class for identity management

---
+++ Java-IdentityManagement-2023/src/main/java/fi/tamk/sade23/idm/Contact.java
@@ -0,0 +1,57 @@
+package fi.tamk.sade23.idm;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    public Contact(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPersonalEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private ContactAttributeStrategy strategy;
+
+    public void setStrategy(ContactAttributeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void applyStrategy() {
+        strategy.setAttributes(this);
+    }
+}
